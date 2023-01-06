e50c491366581ebe7a437c0d86d72d4080121f23
Elias Bermudez <eliasbermudezxiii@gmail.com>
2023-01-06 18:24:06 +0000

    Project doned in class group

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Cards = () => {
+	return (
+    <div className="card-group mx-5 mb-5"> 
+      <div className="card mx-3 border border-dark-subtle rounded">
+        <img src="https://thomasfineart.net/wp-content/uploads/2021/03/John-Clayton-Adams-cr-1-500x325.jpg" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div className="card-footer">
+          <a href="#" className="btn btn-primary">Find Out More!</a>
+        </div>
+      </div>
+      <div className="card mx-3 border border-dark-subtle rounded">
+        <img src="https://thomasfineart.net/wp-content/uploads/2021/03/John-Clayton-Adams-cr-1-500x325.jpg" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div className="card-footer">
+          <a href="#" className="btn btn-primary">Find Out More!</a>
+        </div>
+      </div>
+      <div className="card mx-3 border border-dark-subtle rounded">
+        <img src="https://thomasfineart.net/wp-content/uploads/2021/03/John-Clayton-Adams-cr-1-500x325.jpg" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div className="card-footer">
+          <a href="#" className="btn btn-primary">Find Out More!</a>
+        </div>
+      </div>
+      <div className="card mx-3 border border-dark-subtle rounded">
+        <img src="https://thomasfineart.net/wp-content/uploads/2021/03/John-Clayton-Adams-cr-1-500x325.jpg" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div className="card-footer">
+          <a href="#" className="btn btn-primary">Find Out More!</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const   Jumbotron = () => {
+	return (
+		<div className="jumbotron text-start m-4 mx-5 p-5 bg-light">
+  <h1 className="display-4">A Warm Welcome!</h1>
+  <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita et corrupti quia itaque natus doloremque nesciunt ea blanditiis eum sapiente quas asperiores, voluptatem quaerat ipsam facilis nemo nulla velit minus?</p>
+  <p className="lead">
+    <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+  </p>
+</div>);
+    
+};
+
+export default Jumbotron;
